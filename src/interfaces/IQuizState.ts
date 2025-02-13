3853177c0721d22c62791aa7ac33c9ffb6c09a5d
michaelperryjnr <michaelperryt97@gmail.com>
2025-02-13 19:28:55 +0000

    Added new interfaces

--- src/interfaces/IQuizState.ts
+++ src/interfaces/IQuizState.ts
@@ -1,12 +1,40 @@
-import { IQuizQuestion } from "@/types/quiz";
+interface QuizQuestion {
+  question: "string";
+  options?: "string";
+  answer: "string";
+  type: "string";
+  explanation?: string;
+  lectureNumber: string;
+  hint?: string;
+}
+interface QuizQuestions {
+  name: string;
+  questions: QuizQuestion[];
+}
 
+interface FullQuiz {
+  courseCode: string;
+  quizQuestions: QuizQuestions[] | null;
+}
+
+interface QuizSettings {
+  lectures: string[];
+  showHints: boolean;
+  feedbackType: "during" | "after";
+  timer: number;
+  timerEnabled: boolean;
+  autoNext: boolean;
+  isLinear: boolean;
+  randomizeQuestions: boolean; // Added randomization option
+}
 interface IQuizState {
-  questions: IQuizQuestion[];
+  quiz: FullQuiz | null;
   currentQuestion: number;
+  quizStateSettings: QuizSettings | null;
   answers: string[];
   currentTime: number;
-  userSelectedQuestions: IQuizQuestion[];
+  userSelectedLectures: string[];
   score: number;
 }
 
-export type { IQuizState };
+export type { IQuizState, FullQuiz, QuizSettings };
--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -1,6 +1,13 @@
 import { IAuthState, IUser } from "./IAuthState";
-import { IQuizState } from "./IQuizState";
+import { IQuizState, FullQuiz } from "./IQuizState";
 import { IPaymentState } from "./IPaymentState";
 import ILoginResponse from "./ILoginResponse";
 
-export type { IAuthState, IUser, IQuizState, IPaymentState, ILoginResponse };
+export type {
+  IAuthState,
+  IUser,
+  IQuizState,
+  IPaymentState,
+  ILoginResponse,
+  FullQuiz,
+};
