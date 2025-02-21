1f925a26048094b1412e527e07b39e1603e4b43e
michaelperryjnr <michaelperryt97@gmail.com>
2025-02-21 16:09:53 +0000

    Fixed theme toggle view

--- src/components/theme-toggle.tsx
+++ src/components/theme-toggle.tsx
@@ -15,7 +15,7 @@ export function ThemeToggle() {
       onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
     >
       <Sun className="h-5 w-5 rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0 text-secondary" />
-      <Moon className="absolute h-5 w-5 rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100 text-white" />
+      <Moon className="absolute h-5 w-5 rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100 text-muted-foreground" />
       <span className="sr-only">Toggle theme</span>
     </Button>
   );
